14cbea912c1518db7b58217d7476bf953b359c6c
Praveen Babu <praveenbbu22793@gmail.com>
2022-08-27 20:36:22 +0530

    REST API1 practice

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 
-function App() {
+const App = () => {
+  const [count, setCount] = useState(0);
+
+  const fetch2 = async () => {
+    let c = 0;
+    //as team1
+    await fetch(
+      `https://jsonmock.hackerrank.com/api/football_matches?year=2011&team1=Barcelona`
+    )
+      .then((response) => (response = response.json()))
+      .then((response) => {
+        console.log(response);
+        const matches = response.data;
+        for (let i = 0; i < matches.length; i++) {
+          c += parseInt(matches[i].team1goals);
+        }
+      })
+      .catch((err) => console.log(err));
+    //as team2
+    await fetch(
+      "https://jsonmock.hackerrank.com/api/football_matches?year=2011&team2=Barcelona"
+    )
+      .then((res) => res.json())
+      .then((res) => {
+        const matches = res.data;
+        // console.log(matches);
+        for (let i = 0; i < matches.length; i++) {
+          c += parseInt(matches[i].team2goals);
+        }
+      })
+      .catch((err) => console.log(err));
+    setCount(c);
+    console.log(c);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={fetch2}>Fetch</button>
+      <h3>{count}</h3>
     </div>
   );
-}
+};
 
 export default App;
